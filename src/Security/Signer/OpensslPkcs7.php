0525030cd9b97b631bcd4ee97e06109b04e83aea
a.galeeva <a.galeeva@mcart.ru>
2024-02-16 13:00:14 +0300

    replacing OpensslPkcs7 with cprocsp

--- src/Security/Signer/OpensslPkcs7.php
+++ src/Security/Signer/OpensslPkcs7.php
@@ -9,50 +9,24 @@ class OpensslPkcs7 extends Signer
 {
     public function sign($message)
     {
-        $certContent = file_get_contents($this->certificatePath);
-        $keyContent = file_get_contents($this->privateKeyPath);
-
-        try {
-            set_error_handler(function () {
-                throw SignException::canNotReadCertificate($this->certificatePath);
-            });
-            $cert = openssl_x509_read($certContent);
-        } finally {
-            restore_error_handler();
-        }
-
-        $privateKey = openssl_pkey_get_private($keyContent, $this->privateKeyPassword);
-        if (!is_resource($privateKey)) {
-            throw SignException::canNotReadPrivateKey($this->privateKeyPath);
-        }
-
-        $messageFile = tempnam(sys_get_temp_dir(), 'messageFile');
-        $signFile = tempnam(sys_get_temp_dir(), 'signFile');
-        file_put_contents($messageFile, $message);
-
-        try {
-            set_error_handler(function () {});
-            $signResult = openssl_pkcs7_sign(
-                $messageFile,
-                $signFile,
-                $cert,
-                $privateKey,
-                [],
-                PKCS7_DETACHED | PKCS7_BINARY | PKCS7_NOATTR
-            );
-        } finally {
-            restore_error_handler();
-        }
-        if (false === $signResult) {
-            throw SignException::signFailedAsOf(openssl_error_string());
-        }
-
-        $signed = file_get_contents($signFile);
-        $signed = explode("\n\n", $signed);
-
-        unlink($signFile);
-        unlink($messageFile);
-
-        return base64_decode($signed[3]);
+        try {            
+            $mfr = tempnam ( '/tmp' , 'cspr_' );
+            $mfw = tempnam ( '/tmp' , 'cspw_' );
+            $myfile = fopen($mfr, 'w');
+            fwrite($myfile, $message);
+            fclose($myfile);
+            
+            shell_exec('/opt/cprocsp/bin/amd64/csptest -sfsign -sign -in '.$mfr.' -out '.$mfw.' -my "'.ESIA_KEY_ID.'" -detached -password "'.ESIA_KEY_PASSWORD.'" -add');
+        
+            $myfile = fopen($mfw, "r");
+            $sm = fread($myfile,filesize($mfw));
+            fclose($myfile);
+            unlink($mfr);
+            unlink($mfw);
+        
+            return $sm;
+        } catch (Exception $e) {
+            throw SignException::signFailedAsOf("Ошибка подписи сообщения к ЕСИА!");
+        }    
     }
 }
